6c4f5f94fbb67555c0f86554e573b6f049e26706
MyKhant <myokhantkyaw148@gmail.com>
2023-01-25 15:14:07 +0630

    Upload Book Store project with cart

---
+++ src/main/java/com/example/bookstoredemo/ds/CartBean.java
@@ -0,0 +1,42 @@
+package com.example.bookstoredemo.ds;
+
+import com.example.bookstoredemo.dao.BookDao;
+import com.example.bookstoredemo.entity.Author;
+import com.example.bookstoredemo.entity.Category;
+import jakarta.persistence.Column;
+import jakarta.persistence.ManyToOne;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@SessionScope
+@Component
+public class CartBean {
+
+    private Set<BookDto> bookDtoList =
+            new HashSet<>();
+
+    public void addToCart(BookDto bookDto){
+        bookDtoList.add(bookDto);
+    }
+
+    public Set<BookDto> listAllCart(){
+        return this.bookDtoList;
+    }
+    public void clearCart(){
+        this.bookDtoList.clear();
+    }
+    public int cartSize(){
+        return bookDtoList.size();
+    }
+
+    public void removeCart(BookDto bookDto){
+        bookDtoList.remove(bookDto);
+    }
+}
